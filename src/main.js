72948fc19ada24f59ac3de090469526d7bf007af
GCZ-Zero <1203970284@qq.com>
2022-09-23 13:17:20 +0000

    9/23

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import TDesign from 'tdesign-mobile-vue';
 
 const lightLion = createApp(App)
 lightLion.use(router)
+lightLion.use(TDesign)
 lightLion.mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,17 @@
 <script>
-
+import { ViewList } from "tdesign-icons-vue-next";
+export default {
+  components: { ViewList }
+}
 </script>
 
 <template>
   <div class="home">
-
+    <t-navbar>
+      心电社区
+      <template #right>
+        <view-list></view-list>
+      </template>
+    </t-navbar>
   </div>
 </template>
