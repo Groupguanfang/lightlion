5fa75f64c8cadac338b3460064fbbf1373458f09
GCZ-Zero <1203970284@qq.com>
2022-09-24 11:38:40 +0000

    Okay

--- src/components/Home/AppGird.vue
+++ src/components/Home/AppGird.vue
@@ -13,19 +13,23 @@ export default {
     <t-grid :column="2">
       <t-grid-item
         text="腕上B站"
-        image="@/assets/icon/bilibili.png"
+        image="https://td.xhhzs.cn/wp-content/uploads/2022/09/1664019066-PNG图像.png"
+        layout="horizontal"
       />
       <t-grid-item
         text="腕上资讯"
-        image="../../assets/icon/news.png"
+        image="https://td.xhhzs.cn/wp-content/uploads/2022/09/1664019065-PNG图像-3.png"
+        layout="horizontal"
       />
       <t-grid-item
         text="腕上小说"
-        image="../../assets/icon/novel.png"
+        image="https://td.xhhzs.cn/wp-content/uploads/2022/09/1664019067-PNG图像-2.png"
+        layout="horizontal"
       />
       <t-grid-item
         text="腕上樱花"
-        image="../../assets/icon/bilibili.png"
+        image="https://td.xhhzs.cn/wp-content/uploads/2022/09/1664019066-PNG图像.png"
+        layout="horizontal"
       />
     </t-grid>
   </div>
