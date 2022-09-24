b511c74fef351b01af8355242fdd0cda171117f0
GCZ-Zero <1203970284@qq.com>
2022-09-24 11:19:44 +0000

    add image

--- src/components/Home/AppGird.vue
+++ src/components/Home/AppGird.vue
@@ -1,13 +1,32 @@
 <script>
-
+export default {
+  data() {
+    return {
+      img1url: "@/assets/icon/bilibili.png"
+    }
+  }
+}
 </script>
 
 <template>
   <div class="app-gird">
-    <t-grid :column="2" :border="true">
-      <t-gird-item>
-        
-      </t-gird-item>
-    </t-gird>
+    <t-grid :column="2">
+      <t-grid-item
+        text="腕上B站"
+        image="@/assets/icon/bilibili.png"
+      />
+      <t-grid-item
+        text="腕上资讯"
+        image="../../assets/icon/news.png"
+      />
+      <t-grid-item
+        text="腕上小说"
+        image="../../assets/icon/novel.png"
+      />
+      <t-grid-item
+        text="腕上樱花"
+        image="../../assets/icon/bilibili.png"
+      />
+    </t-grid>
   </div>
 </template>
