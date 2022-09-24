e329478a305fa64187e999c922802c6f93527629
GCZ-Zero <1203970284@qq.com>
2022-09-24 09:57:58 +0000

    add code

--- src/components/Home/Swiper.vue
+++ src/components/Home/Swiper.vue
@@ -1,9 +1,19 @@
 <script>
-
+export default {
+  data() {
+    return {
+      img1url:"./assets/default-swiper.jpeg"
+    }
+  }
+}
 </script>
 
 <template>
   <div class="home-swiper">
-    
+    <t-swiper>
+      <t-swiper-item>
+        <img src="/assets/default-swiper.jpeg" />
+      </t-swiper-item>
+    </t-swiper>
   </div>
 </template>
