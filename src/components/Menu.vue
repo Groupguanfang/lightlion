6d0ee2d806d19dccd2f5ad597eb1489e6fd87c76
GCZ-Zero <1203970284@qq.com>
2022-09-24 12:30:12 +0000

    Update Menu.vue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,25 +2,40 @@
 export default {
   data() {
     return {
-      openBase: false
-    }
+      openBase: false,
+      menuItems: [
+        {
+          title: "首页",
+          key: "/",
+        },
+      ],
+      itemClick: (index, item, context) => {
+        this.$router.push(item.key);
+      },
+    };
+  },
+  methods: {
+    clickAction(index, item) {
+      console.log(index);
+    },
   },
   watch: {
-    '$store.state.isMenuOpen'() {
-      if (this.$store.state.isMenuOpen)
-      {
-        this.openBase = true
+    "$store.state.isMenuOpen"() {
+      if (this.$store.state.isMenuOpen) {
+        this.openBase = true;
       } else {
-        this.openBase = false
+        this.openBase = false;
       }
-    }
+    },
   },
-}
+};
 </script>
 
 <template>
   <t-drawer
+    @item-click="itemClick"
     v-model:visible="openBase"
     placement="left"
+    :items="menuItems"
   />
 </template>
