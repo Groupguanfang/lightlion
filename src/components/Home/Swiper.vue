8fad55f553640546440230fedf25cdb972608c84
GCZ-Zero <1203970284@qq.com>
2022-09-24 10:28:41 +0000

    size changed

--- src/components/Home/Swiper.vue
+++ src/components/Home/Swiper.vue
@@ -2,7 +2,6 @@
 export default {
   data() {
     return {
-      img1url:"./assets/default-swiper.jpeg"
     }
   }
 }
@@ -11,9 +10,21 @@ export default {
 <template>
   <div class="home-swiper">
     <t-swiper>
-      <t-swiper-item>
-        <img src="../../assets/default-swiper.jpeg" />
+      <t-swiper-item class="item">
+        <img src="../../assets/default-swiper.jpeg" class="img"/>
       </t-swiper-item>
     </t-swiper>
   </div>
 </template>
+
+<style>
+.item {
+  height:100%;
+  width: 100%;
+  object-fit: cover;
+}
+.img {
+  height: 100%;
+  width:100%;
+}
+</style>
