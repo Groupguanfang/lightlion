fbf5c415d460ee35dade82170f1cc3088a2b59c3
GCZ-Zero <1203970284@qq.com>
2022-09-24 10:58:25 +0000

    add appgird

---
+++ src/components/Home/AppGird.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <div class="app-gird">
+    <t-grid :column="2" :border="true">
+      <t-gird-item>
+        
+      </t-gird-item>
+    </t-gird>
+  </div>
+</template>
