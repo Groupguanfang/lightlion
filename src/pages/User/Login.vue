b61db852a312fa3305b13b9b8837802be16eda8a
GCZ-Zero <1203970284@qq.com>
2022-09-27 13:00:19 +0000

    login is success！

--- src/pages/User/Login.vue
+++ src/pages/User/Login.vue
@@ -1,16 +1,29 @@
 <script>
 import { UserAddIcon } from "tdesign-icons-vue-next"
+import { loginAction } from "../../api/User";
 export default {
   components: { UserAddIcon },
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+      buttonLoading: false
     }
   },
   methods: {
-    loginAcion() {
+    async loginActionTo() {
+      this.buttonLoading = true;
+      const action = await loginAction(this.username,this.password);
+      console.log(action.data);
       
+      if (action.data.code !== 500) {
+        localStorage.setItem('token',action.data[0].token);
+        this.$router.push('/usercenter')
+      } else {
+        this.$toast(action.data.message)
+      }
+      
+      this.buttonLoading = false;
     }
   }
 };
@@ -31,7 +44,7 @@ export default {
       <h1>登录</h1>
       <t-input v-model="username" label="用户名" placeholder="请输入用户名"></t-input>
       <t-input v-model="password" label="密码" placeholder="请输入密码"></t-input>
-      <t-button block style="margin-top: 10px" theme="primary" @click="">登录</t-button>
+      <t-button :loading="buttonLoading" block style="margin-top: 10px" theme="primary" @click="loginActionTo()">登录</t-button>
     </main>
   </div>
 </template>
