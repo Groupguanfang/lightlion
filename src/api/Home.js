9863e9315def5d7c8c19c7d77a9be73e630cbca6
GCZ-Zero <1203970284@qq.com>
2022-09-27 07:08:00 +0000

    yyy

--- src/api/Home.js
+++ src/api/Home.js
@@ -2,7 +2,10 @@ import axios from "../utils/axios";
 
 /**
  * 获取文章接口
- * @returns [json]
+ *
+ * @returns [object array] post list
+ * @author Zero
+ * @since 2022
  */
 export async function getPost() {
   let data = await axios.get("/post");
--- src/components/Discover/PostList.vue
+++ src/components/Discover/PostList.vue
@@ -8,8 +8,8 @@ export default {
     }
   },
   async mounted() {
-    let postdata = await getPost().data
-    this.post = postdata
+    let postdata = await getPost()
+    this.post = postdata.data
     console.log(this.post)
   }
 }
@@ -17,10 +17,17 @@ export default {
 
 <template>
   <div class="post-list">
+  <!--
     <t-list>
-      <t-cell v-for="(item,index) in post">
-        {{ item.title }}
+      <t-cell v-for="(item,index) in post" :key="index">
+      {{ item.title }}
       </t-cell>
     </t-list>
+  -->
+  <t-list>
+    <t-cell v-for="item in post" :key="item">
+      <span class="cell">{{ item.title }}</span>
+    </t-cell>
+  </t-list>
   </div>
 </template>
