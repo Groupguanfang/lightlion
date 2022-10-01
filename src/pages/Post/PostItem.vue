48a2c1edfc7b3013289a1409ef5a89800e7b3f17
GCZ-Zero <1203970284@qq.com>
2022-10-01 03:25:09 +0000

    Update PostItem.vue

--- src/pages/Post/PostItem.vue
+++ src/pages/Post/PostItem.vue
@@ -1,8 +1,9 @@
 <script>
 import { getPostItem } from "../../api/Home";
-import { WifiIcon } from "tdesign-icons-vue-next";
+import { WifiIcon, UserIcon } from "tdesign-icons-vue-next";
 import { h } from "vue";
 export default {
+  components: { WifiIcon, UserIcon },
   data() {
     return {
       data: {},
@@ -32,17 +33,65 @@ export default {
         direction: "column",
       });
     }
+
+    document
+      .querySelector(".root")
+      .addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      const height = document.querySelector(".root").scrollTop;
+      if (height >= 200) {
+        document.querySelector("#navbar").classList.remove("hide");
+        document.querySelector("#navbar").classList.add("show");
+      } else {
+        document.querySelector("#navbar").classList.remove("show");
+        document.querySelector("#navbar").classList.add("hide");
+      }
+    },
   },
 };
 </script>
 
 <template>
   <div class="post-item">
-    <t-navbar>{{ title }}</t-navbar>
+    <t-navbar class="header hide" id="navbar">{{ data.title }}</t-navbar>
     <t-loading theme="bar" :progress="progress" />
-    <div class="content">
-      <h1>{{ data.title }}</h1>
+    <div class="content padding">
+      <div class="content-header">
+        <h1 class="title">{{ data.title }}</h1>
+        <div class="author">
+          <t-avatar>
+            <template #icon>
+              <UserIcon />
+            </template>
+          </t-avatar>
+          <div class="info">
+              {{ data.author }}
+          </div>
+        </div>
+      </div>
       {{ data.data }}
     </div>
   </div>
 </template>
+
+<style>
+.header {
+  position: fixed;
+  width: 100%;
+  transition: ease 0.2s;
+  box-shadow: 0 0 10px #88888852;
+}
+.content-header .author {
+  margin-block-end: 0.67em;
+}
+.show {
+  transform: translateY(0);
+  transition: ease 0.2s;
+}
+.hide {
+  transform: translateY(-100px);
+  transition: ease 0.2s;
+}
+</style>
