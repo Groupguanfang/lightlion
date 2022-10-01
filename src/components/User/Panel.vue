d53876abe5f2f38287ef9dc85627f39d9b4389b2
GCZ-Zero <1203970284@qq.com>
2022-10-01 06:26:19 +0000

    111

---
+++ src/components/User/Panel.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  props: ["follow", "fans", "money"],
+};
+</script>
+
+<template>
+  <div class="panel">
+    <div class="info padding">
+      <div class="data-container">
+        <div class="item">
+          <div class="data">{{ follow }}</div>
+          <div class="title">关注</div>
+        </div>
+        <div class="item">
+          <div class="data">{{ fans }}</div>
+          <div class="title">粉丝</div>
+        </div>
+        <div class="item">
+          <div class="data">{{ money }}</div>
+          <div class="title">积分</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.info {
+  padding-top: 0;
+}
+.data-container {
+  padding: 14px;
+  display: flex;
+  justify-content: space-around;
+  border-radius: 8px;
+  background: #eee;
+}
+.data-container .item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.data-container .item .title {
+  font-size: 14px;
+  color: #888;
+}
+.data-container .item .data {
+  font-size: 18px;
+}
+</style>
--- src/pages/User/UserCenter.vue
+++ src/pages/User/UserCenter.vue
@@ -1,11 +1,18 @@
 <script>
 import { getInfo } from "../../api/User";
 import TabBar from "../../components/TabBar.vue";
+import { UserIcon } from "tdesign-icons-vue-next";
+import Panel from "../../components/User/Panel.vue";
 export default {
-  components: { TabBar },
+  components: { TabBar, UserIcon, Panel },
   data() {
     return {
-      userInfo: {},
+      loading: true,
+      maincontent: false,
+      userInfo: {
+        userdata: { id: "", name: "", avatar: "", saying: "" },
+        profile: { id: "", level: "", today: "", phone: "" },
+      },
       showActionSheet: false,
       items: [
         {
@@ -19,15 +26,20 @@ export default {
       ],
     };
   },
-  async mounted() {
-    const info = await getInfo(localStorage.getItem("token"));
-    this.userInfo = info;
-    console.log(this.userInfo);
+  async created() {
+    try {
+      const info = await getInfo(localStorage.getItem("token"));
+      this.userInfo = info.data;
+      this.loading = false;
+      this.maincontent = true;
+    } catch (err) {
+      this.$toast(err);
+    }
   },
   methods: {
     handleSelected(selected, selectedIndex) {
       if (selectedIndex === 0) {
-        
+        this.$toast("已登出");
       }
     },
   },
@@ -44,5 +56,66 @@ export default {
       :items="items"
       v-model="showActionSheet"
     />
+
+    <!-- 骨架屏 -->
+    <div class="container padding" v-if="loading">
+      <div class="headerCard">
+        <div class="left">
+          <t-skeleton animation="gradient" theme="avatar" />
+        </div>
+        <div class="right">
+          <t-skeleton
+            :rowCol="[{ width: '100px' }, 2, 1]"
+            theme="text"
+            animation="gradient"
+          ></t-skeleton>
+        </div>
+      </div>
+    </div>
+
+    <!-- 真正的内容 -->
+    <div class="container padding" v-if="maincontent">
+      <div class="headerCard">
+        <div class="left">
+          <t-avatar size="large">
+            <template #icon>
+              <UserIcon />
+            </template>
+          </t-avatar>
+        </div>
+        <div class="right">
+          <div class="name">{{ userInfo.userdata.name }}</div>
+          <div class="tags" theme="primary">ID：{{ userInfo.userdata.id }}</div>
+          <div class="saying">
+            {{ userInfo.userdata.saying }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Panel follow="0" fans="0" money="0" />
   </div>
 </template>
+
+<style scoped>
+.headerCard {
+  padding: 14px;
+  padding-bottom: 0;
+  display: flex;
+  gap: 14px;
+  clear: both;
+}
+.headerCard .right {
+  width: 100%;
+  clear: both;
+}
+
+.headerCard .right .name {
+  font-size: 20px;
+}
+
+.headerCard .right .tags {
+  clear: both;
+  width: 100%;
+}
+</style>
