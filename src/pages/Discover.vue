0fb4b7ebf7c83ea1ba6077ccfdf95f247fb6276e
GCZ-Zero <1203970284@qq.com>
2022-10-01 07:58:30 +0000

    nice

--- src/pages/Discover.vue
+++ src/pages/Discover.vue
@@ -2,13 +2,15 @@
 import PostList from "../components/Discover/PostList.vue";
 import TabBar from "../components/TabBar.vue";
 export default {
-  components: { TabBar, PostList }
-}
+  components: { TabBar, PostList },
+};
 </script>
 
 <template>
-  <div class="discover padding">
-    <h1>发现</h1>
+  <div class="discover">
+    <div class="padding">
+      <h1>发现</h1>
+    </div>
     <PostList />
     <TabBar />
   </div>
