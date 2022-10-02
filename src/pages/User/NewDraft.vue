843e9bcbf0ac61a5722b48d9583552a6a05c39ea
GCZ-Zero <1203970284@qq.com>
2022-10-02 13:57:35 +0000

    add newdraft

--- src/pages/User/NewDraft.vue
+++ src/pages/User/NewDraft.vue
@@ -1,19 +1,51 @@
 <script>
-import MdEditor from "md-editor-v3";
-import "md-editor-v3/lib/style.css";
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+import empty from "../../utils/empty";
+
 export default {
-  conponents: { MdEditor },
+  components: { MdEditor },
   data() {
     return {
       text: "",
-    };
+      html: ""
+    }
   },
-};
+  mounted() {
+    if (empty(localStorage.getItem('token'))) {
+      this.$router.push("/")
+      this.$toast("登录状态已失效，请重新登录")
+    }
+  },
+  methods: {
+    getHtml(html) {
+      this.html = html
+    },
+    saved(params) {
+      this.$toast(params)
+    }
+  }
+}
 </script>
 
 <template>
   <div class="newdraft">
     <t-navbar>新建草稿</t-navbar>
-    <md-editor v-model="text" />
+    <md-editor
+      v-model="text"
+      :noKatex="true"
+      :toolbars="toolbars"
+      @on-Html-changed="getHtml"
+      @on-save="saved"
+    />
+
+    <div>
+      raw数据：
+      {{ text }}
+    </div>
+    <div>
+      html数据：
+      {{ html }}
+    </div>
   </div>
 </template>
