0a518f84cf89e2b47611c4ce7b0b6df58450611f
GCZ-Zero <1203970284@qq.com>
2022-10-03 12:19:30 +0000

    today new build

--- src/pages/User/Settings.vue
+++ src/pages/User/Settings.vue
@@ -1,11 +1,23 @@
 <script>
+import SettingPassword from "../../components/User/SettingPassword.vue"
+
 export default {
-  
+  components: { SettingPassword }
 }
 </script>
 
 <template>
   <div class="settings">
-    
+    <t-navbar :rightShow="false">
+      设置
+    </t-navbar>
+  
+    <t-tabs default-value="password">
+        <t-tab-panel value="password" label="通用">
+          <SettingPassword />
+        </t-tab-panel>
+        <t-tab-panel value="developer" label="开发者Debug">
+        </t-tab-panel>
+      </t-tabs>
   </div>
 </template>
