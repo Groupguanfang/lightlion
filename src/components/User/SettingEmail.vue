b67448dddf88dd26f5c6da91d43c35bda3cdf0fa
GCZ-Zero <1203970284@qq.com>
2022-10-05 14:28:23 +0000

    new password

--- src/components/User/SettingEmail.vue
+++ src/components/User/SettingEmail.vue
@@ -7,47 +7,59 @@ export default {
       code: "",
       buttonText: "发送验证码",
       buttonDisabled: false,
-      oldMail: "正在加载中..."
-    }
+      oldMail: "正在加载中...",
+    };
   },
   async mounted() {
-    const info = await getInfo(localStorage.getItem('token'))
-    this.oldMail = info.data.userdata.email
+    const info = await getInfo(localStorage.getItem("token"));
+    this.oldMail = info.data.userdata.email;
   },
   methods: {
     async action() {
-      this.buttonDisabled = true
-      this.buttonText = "正在发送"
-      const data = await sendEmail(this.newMail)
-      console.log(data)
-      this.buttonDisabled = false
-      this.$toast(data.data.message)
-      this.buttonText = "重新发送"
+      this.buttonDisabled = true;
+      this.buttonText = "正在发送";
+      const data = await sendEmail(this.newMail);
+      console.log(data);
+      this.buttonDisabled = false;
+      this.$toast(data.data.message);
+      this.buttonText = "重新发送";
     },
     async variety() {
-      const nextData = await changeMail(localStorage.getItem('token'),this.newMail,this.code)
-      console.log(nextData)
-      this.$toast(nextData.data.message)
-      if (nextData.data.code == 200)
-      {
-        this.$router.push('/usercenter')
+      const nextData = await changeMail(
+        localStorage.getItem("token"),
+        this.newMail,
+        this.code
+      );
+      console.log(nextData);
+      this.$toast(nextData.data.message);
+      if (nextData.data.code == 200) {
+        this.$router.push("/usercenter");
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
   <div class="set-email padding">
     <div class="old">当前邮箱地址：{{ oldMail }}</div>
-    <t-input v-model="newMail" label="新邮箱地址" placeholder="请输入新的电子邮件地址"/>
+    <t-input
+      v-model="newMail"
+      label="新邮箱地址"
+      placeholder="请输入新的电子邮件地址"
+    />
     <t-input v-model="code" label="验证码" placeholder="请输入验证码">
       <template #suffix>
-        <t-button @click="action()" :disabled="buttonDisabled" variant="text">
+        <t-button
+          theme="primary"
+          @click="action()"
+          :disabled="buttonDisabled"
+          variant="text"
+        >
           {{ buttonText }}
         </t-button>
       </template>
     </t-input>
-    <t-button @click="variety()" block>保存</t-button>
+    <t-button @click="variety()" block theme="primary">保存</t-button>
   </div>
 </template>
--- src/components/User/SettingPassword.vue
+++ src/components/User/SettingPassword.vue
@@ -1,17 +1,31 @@
 <script>
+import empty from "../../utils/empty";
+import { changePwd } from "../../api/User";
 export default {
   data() {
     return {
-      oldPassword: "",
-      newPassword: "",
+      password: "",
+      passwordRetry: "",
     };
   },
   methods: {
-    action() {
-      this.$toast("正在开发中")
-      if (this.oldPassword === this.newPassword) {
-        this.$message.error("新密码不能与旧密码相同");
+    async action() {
+      if (empty(this.password)) {
+        this.$toast("请输入新密码");
+        return;
       }
+
+      if (empty(this.passwordRetry)) {
+        this.$toast("请再次输入密码QWQ");
+      }
+
+      if (this.password !== this.passwordRetry) {
+        this.$toast("两次密码不一致");
+        return;
+      }
+
+      const ckr = await changePwd(this.password, localStorage.getItem("token"));
+      this.$toast(ckr.data.message)
     },
   },
 };
@@ -20,17 +34,17 @@ export default {
 <template>
   <div class="set-password padding">
     <t-input
-      v-model="oldPassword"
-      type="password"
-      label="旧密码"
-      placeholder="必填"
-    />
-    <t-input
-      v-model="newPassword"
+      v-model="password"
       type="password"
       label="新密码"
       placeholder="必填"
     />
-    <t-button @click="action()" block theme="primary"> 保存 </t-button>
+    <t-input
+      v-model="passwordRetry"
+      type="password"
+      label="重新密码"
+      placeholder="必填"
+    />
+    <t-button @click="action()" block theme="primary">保存</t-button>
   </div>
 </template>
