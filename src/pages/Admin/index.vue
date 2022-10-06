96cf92ee338d48d11e23d3dc11d7f28a42cf2715
GCZ-Zero <1203970284@qq.com>
2022-10-06 06:45:51 +0000

    yysy

--- src/pages/Admin/index.vue
+++ src/pages/Admin/index.vue
@@ -1,13 +1,13 @@
 <script>
 import { getInfo } from "../../api/User";
-import AnnouncementVue from "./Parts/Home/Announcement.vue";
+import Home from "./Parts/Home.vue";
 export default {
-  components: { AnnouncementVue },
+  components: { Home },
   async mounted() {
     const control = await getInfo(localStorage.getItem("token"));
-    if (control.data.profile.level != 10) {
-      this.$router.go(-1);
-      this.$toast.fail("您无权访问该页面");
+    if (control.data.userdata.level != 10) {
+      this.$router.push('/');
+      this.$toast("您无权访问该页面");
     }
   },
   methods: {},
@@ -15,27 +15,12 @@ export default {
 </script>
 
 <template>
-  <div class="admin-index padding">
-    <h1>管理主页</h1>
+  <div class="admin-index">
+    <h1 class="padding">管理主页</h1>
     <t-tabs default-value="general">
-      <t-tab-panel class="padding" value="general" label="通用">
-        <AnnouncementVue />
-      </t-tab-panel>
-      <t-tab-panel class="padding" value="about" label="关于">
-        <div class="about">前端：Zero 后端：Zero Copyright Since 2022</div>
+      <t-tab-panel value="general" label="通用">
+        <Home />
       </t-tab-panel>
     </t-tabs>
   </div>
 </template>
-
-<style>
-.admin-index {
-  background: #f6f6f6;
-  height: 100vh;
-}
-
-.admin-index .setting-item .t-input,
-.admin-index .setting-item .t-button {
-  border-radius: 10px;
-}
-</style>
