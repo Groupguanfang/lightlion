e10dd596cc79e9f1738824d964769c1bf33ba943
GCZ-Zero <1203970284@qq.com>
2022-10-16 05:26:53 +0000

    666

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import TDesign from "tdesign-mobile-vue";
-import "./style.css";
+import "./style.less";
 
 const lightLion = createApp(App);
 lightLion.use(router);
